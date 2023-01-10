eb5e3b49c49a744c0ea80b6fb24f0188d426e3b5
douryne <douryne.1st@gmail.com>
2023-01-10 22:13:14 +0400

    posts and post

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,42 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  type Post = {
+    id: number,
+    title: string,
+    body: string
+  }
+
+  const posts = ref<Post[]>([
+    {id: 1, title: "Title 1", body: "Body 1"},
+    {id: 2, title: "Title 2", body: "Body 2"},
+    {id: 3, title: "Title 3", body: "Body 3"},
+  ])
+</script>
+
 <template>
-  Works!
+  <div>
+    <div class="posts" v-if="posts">
+      <div class="post" v-for="post in posts">
+        <span><strong>Title: </strong>{{ post.title }}</span> 
+        <span><strong>Body: </strong>{{ post.body }}</span> 
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+  .posts {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 15px;
+  }
+  .post {
+    display: flex;
+    flex-direction: column;
+    row-gap: 5px;
+    padding: 15px;
+    border: 2px solid teal;
+    border-radius: 15px;
+  }
+</style>
