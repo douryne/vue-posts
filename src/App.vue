f4c4eb9149f98837cddae7778fe471a0e5801b4f
douryne <douryne.1st@gmail.com>
2023-01-11 15:59:12 +0400

    Dialog component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
     import { ref } from 'vue';
     import PostFormVue from './components/PostForm.vue';
     import PostListVue from './components/PostList.vue';
+    import DialogVue from './components/UI/Dialog.vue';
+    import ButtonVue from './components/UI/Button.vue';
     import type { Post } from "@/components/PostItem.vue";
 
     const posts = ref<Post[]>([
@@ -9,11 +11,21 @@
         {id: 2, title: "Title 2", body: "Body 2"},
         {id: 3, title: "Title 3", body: "Body 3"},
     ])
+
+    const dialogVisible = ref(false);
+
+    function showDialog() {
+        dialogVisible.value = true
+    }
 </script>
 
 <template>
     <div class="app">
-        <PostFormVue @on-post-creation="(post) => posts.push(post)" />
+        <h1>Posts Page</h1>
+        <ButtonVue @click="showDialog" class="create-button">Create Post</ButtonVue>   
+        <DialogVue v-model:show="dialogVisible">
+            <PostFormVue @on-post-creation="(post) => posts.push(post)" />
+        </DialogVue>
         <PostListVue :posts="posts" @on-delete="(postId) => posts = posts.filter((post) => postId !== post.id)"/>
     </div>
 </template>
@@ -24,5 +36,8 @@
         display: flex;
         flex-direction: column;
         row-gap: 20px;
+    }
+    .app .create-button {
+        align-self: start;
     }
 </style>
---
+++ src/components/UI/Dialog.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+    const props = withDefaults(defineProps<{ show: boolean }>(), {
+        show: false
+    });
+    const emit = defineEmits<{ (event: "update:show", value: boolean): void }>();
+
+    function hideDialog() {
+        emit("update:show", false);
+    }
+</script>
+
+<template>
+    <div v-if="props.show" class="dialog" @click="hideDialog">
+        <div class="content" @click.stop>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .dialog {
+        position: fixed;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+    }
+    .dialog .content {
+        background-color: white;
+        padding: 20px;
+        border-radius: 15px;
+        margin: auto;
+    }
+</style>
